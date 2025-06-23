7591582f87ea0e5cd6d508a7388a1ed32689edf8
AnriAko <ejik.v.tu.m.ane97@gmail.com>
2025-06-23 21:22:05 +0400

    updated for work with db via sys args

--- pdf_text_without_format_to_json_stream.py
+++ pdf_text_without_format_to_json_stream.py
@@ -24,7 +24,7 @@ def extract_text_from_stream(pdf_bytes):
         "p": pages
     }
 
-def save_to_mongodb(data, user_id, mongo_uri, db_name="ol_pdf_to_json", collection_name="pdf_to_json_books"):
+def save_to_mongodb(data, user_id, mongo_uri, db_name, collection_name):
     client = MongoClient(mongo_uri)
     try:
         db = client[db_name]
@@ -37,16 +37,18 @@ def save_to_mongodb(data, user_id, mongo_uri, db_name="ol_pdf_to_json", collecti
         print("MongoDB connection closed")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: cat file.pdf | python script.py <userId> <mongo_uri>")
+    if len(sys.argv) < 5:
+        print("Usage: cat file.pdf | python script.py <userId> <mongo_uri> <db_name> <collection_name>")
         sys.exit(1)
 
     user_id = sys.argv[1]
     mongo_uri = sys.argv[2]
+    db_name = sys.argv[3]
+    collection_name = sys.argv[4]
 
     pdf_bytes = sys.stdin.buffer.read()
 
     start = time.time()
     data = extract_text_from_stream(pdf_bytes)
-    save_to_mongodb(data, user_id, mongo_uri)
+    save_to_mongodb(data, user_id, mongo_uri, db_name, collection_name)
     print(f"Done in {round(time.time() - start, 2)} seconds")
