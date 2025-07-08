71d50681fd02605255f18e2ad09446a999f12012
AnriAko <ejik.v.tu.m.ane97@gmail.com>
2025-07-08 16:39:17 +0400

    filename is now pdf name

--- pdf_text_without_format_to_json_stream.py
+++ pdf_text_without_format_to_json_stream.py
@@ -4,22 +4,22 @@ import sys
 from pymongo import MongoClient
 from io import BytesIO
 import time
+import os
 
-def extract_text_from_stream(pdf_bytes):
+def extract_text_from_stream(pdf_bytes, file_name_with_ext):
     pages = []
     pdf_buffer = BytesIO(pdf_bytes)
-    
+
     with pdfplumber.open(pdf_buffer) as pdf:
         for page in pdf.pages:
             text = page.extract_text()
             pages.append({"t": text})
 
-    pdf_buffer.seek(0)
-    doc = fitz.open(stream=pdf_buffer, filetype="pdf")
-    pdf_title = doc.metadata.get("title") or "Untitled.pdf"
+    # Убираем расширение, если есть
+    base_name = os.path.splitext(os.path.basename(file_name_with_ext))[0]
 
     return {
-        "b": pdf_title,
+        "b": base_name,
         "p_count": len(pages),
         "p": pages
     }
@@ -37,18 +37,19 @@ def save_to_mongodb(data, user_id, mongo_uri, db_name, collection_name):
         print("MongoDB connection closed")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 5:
-        print("Usage: cat file.pdf | python script.py <userId> <mongo_uri> <db_name> <collection_name>")
+    if len(sys.argv) < 6:
+        print("Usage: cat file.pdf | python script.py <userId> <mongo_uri> <db_name> <collection_name> <file_name>")
         sys.exit(1)
 
     user_id = sys.argv[1]
     mongo_uri = sys.argv[2]
     db_name = sys.argv[3]
     collection_name = sys.argv[4]
+    file_name = sys.argv[5]
 
     pdf_bytes = sys.stdin.buffer.read()
 
     start = time.time()
-    data = extract_text_from_stream(pdf_bytes)
+    data = extract_text_from_stream(pdf_bytes, file_name)
     save_to_mongodb(data, user_id, mongo_uri, db_name, collection_name)
     print(f"Done in {round(time.time() - start, 2)} seconds")
